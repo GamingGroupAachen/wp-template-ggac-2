506496139616e16dd2f59774f4bcee5bba4a6987
Aleksandr <schurix@gmx.de>
2016-04-18 14:30:14 +0200

    Changed box

--- template-parts/content-ggac_game.php
+++ template-parts/content-ggac_game.php
@@ -10,33 +10,46 @@
 	</header><!-- .entry-header -->
 
 	<?php twentysixteen_post_thumbnail(); ?>
-	
-	<div class="game-meta panel panel-default">
-		<div class="meta-title panel-heading">Snell-Infos</div>
-		<div class="meta-content panel-body">
-			<div class="ingame-group">
-				<div class="meta-label"><?php _e('Chatroom:') ?></div>
-				<div class="meta-innerContent"><?php the_field('ingame_gruppe'); ?></div>
-			</div>
-			<div class="mailinglist">
-				<div class="meta-label"><?php _e('Orga:') ?></div>
-				<div class="meta-innerContent"><a href="mailto:<?php the_field('mailingliste'); ?>"><?php the_field('mailingliste'); ?></a></div>
-			</div>
-			<div class="ts_tournament">
-				<div class="meta-label"><?php _e('TS Turnier:') ?></div>
-				<div class="meta-innerContent"><?php the_field('ts_turnierserver'); ?></div>
-			</div>
-			<div class="ts_community">
-				<div class="meta-label"><?php _e('TS Community:') ?></div>
-				<div class="meta-innerContent"><?php the_field('ts_communityserver'); ?></div>
-			</div>
-			<div class="facebook">
-				<div class="meta-label"><?php _e('Facebook:') ?></div>
-				<div class="meta-innerContent"><a href="<?php the_field('facebookgruppe'); ?>">FB-Gruppe</a></div>
+	<?php 
+		$boxFields = array(
+			'ingame_gruppe' => 'Chatroom:',
+			'mailingliste' => 'Orga:',
+			'ts_turnierserver' => 'TS Turnier:',
+			'ts_communityserver' => 'TS Community:',
+			'facebookgruppe' => 'Facebook:',
+		); 
+		$nonEmptyField = false;
+		foreach($boxFields as $key => $value) {
+			$fieldValue = get_field($key);
+			if(!empty($fieldValue)){
+				$nonEmptyField = true;
+				break;
+			}
+		}
+		if($nonEmptyField) :
+		?>
+		<div class="game-meta panel panel-default">
+			<div class="meta-content panel-body">
+				<?php foreach($boxFields as $key => $value) :  ?>
+				<?php
+					$fieldValue = get_field($key);
+					if(empty($fieldValue)){
+						continue;
+					}
+				?>
+				<div class="meta-field <?php echo $key ?>">
+					<?php if($key == 'mailingliste') : ?>
+						<div class="meta-label"><?php _e($value) ?></div>
+						<div class="meta-innerContent"><a href="mailto:<?php the_field($key); ?>"><?php the_field($key); ?></a></div>
+					<?php else: ?>
+						<div class="meta-label"><?php _e($value) ?></div>
+						<div class="meta-innerContent"><?php the_field($key); ?></div>
+					<?php endif; ?>
+				</div>
+				<?php endforeach; ?>
 			</div>
 		</div>
-	</div>
-	
+	<?php endif; ?>
 	<div class="entry-content">
 		<?php
 		the_content();
